3fa0ba2d3a562e433709026fa7f38af650ac9124
nikolay.buzdalov <buzdalov@restream.rt.ru>
2018-04-01 00:30:49 +0400

    add buttons, promo block

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 import { hasTouch } from 'detect-touch';
 
+const STICKY_HEADER_TRIGGER = 50;
+const HEADER_CLASSNAME = 'navigation';
+const STICKY_HEADER_CLASSNAME = 'fixed';
+
 if (hasTouch) {
   document.body.classList.add('touchscreen');
 }
+
+const handleScroll = () => {
+  const navigation = document.querySelector(`.${HEADER_CLASSNAME}`);
+  const action = (window.scrollY >= STICKY_HEADER_TRIGGER) ? 'add' : 'remove';
+  navigation.classList[action](STICKY_HEADER_CLASSNAME);
+};
+
+window.addEventListener('scroll', handleScroll);
 
 console.log('test', hasTouch);
