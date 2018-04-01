ff27ec0067f36667013755d8a5474397f02da3dd
nikolay.buzdalov <buzdalov@restream.rt.ru>
2018-04-01 23:39:27 +0400

    add footer, terms, contacts

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import { hasTouch } from 'detect-touch';
 
 const STICKY_HEADER_TRIGGER = 50;
-const HEADER_CLASSNAME = 'navigation';
+const STICKY_HEADER_HEIGHT = 108;
 const STICKY_HEADER_CLASSNAME = 'fixed';
+const HEADER_CLASSNAME = 'navigation';
+const UP_BUTTON_CLASSNAME = '.arrow-up';
 
 if (hasTouch) {
   document.body.classList.add('touchscreen');
@@ -14,6 +16,25 @@ const handleScroll = () => {
   navigation.classList[action](STICKY_HEADER_CLASSNAME);
 };
 
-window.addEventListener('scroll', handleScroll);
+const handleScrollUp = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+};
 
-console.log('test', hasTouch);
+const handleSkipPromo = () => {
+  const promoHeight = document.querySelector('.promo').clientHeight;
+  window.scrollTo({ top: promoHeight - STICKY_HEADER_HEIGHT, left: 0, behavior: 'smooth' });
+};
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
+
+const skipPromo = document.querySelector('.promo__arrow-down');
+if (skipPromo) {
+  skipPromo.addEventListener('click', handleSkipPromo);
+}
+
+const upArrow = document.querySelector(UP_BUTTON_CLASSNAME);
+if (upArrow) {
+  upArrow.addEventListener('click', handleScrollUp);
+}
+
