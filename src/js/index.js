24958f945a118adbbe9e978d8efdb321599d4b5d
nikolay.buzdalov <buzdalov@restream.rt.ru>
2018-04-03 00:13:42 +0400

    add js events handlers

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,77 @@
 import { hasTouch } from 'detect-touch';
 
 const STICKY_HEADER_TRIGGER = 50;
-const STICKY_HEADER_HEIGHT = 108;
+const STICKY_HEADER_HEIGHT = 88;
 const STICKY_HEADER_CLASSNAME = 'fixed';
 const HEADER_CLASSNAME = 'navigation';
 const UP_BUTTON_CLASSNAME = '.arrow-up';
+const DOWN_BUTTON_CLASSNAME = '.promo__arrow-down';
+const NAV_TOGGLE_ICON = '.nav-toggle';
+const HEADER_NAV = '.header-nav';
+const PROMO_BLOCK = '.promo';
+const BTN_TERMS = '#btn-terms';
+const BTN_CONTACTS = '#btn-contacts';
+
+const addEvent = (selector, event, handler) => {
+  const el = document.querySelector(selector);
+  if (el) {
+    el.addEventListener(event, handler);
+  }
+};
+
+const verticalSmoothScroll = (to) => {
+  window.scrollTo({ top: to, left: 0, behavior: 'smooth' });
+};
+
+const scrollToSection = (selector) => {
+  const section = document.querySelector(selector);
+  if (section) {
+    verticalSmoothScroll(section.offsetTop - STICKY_HEADER_HEIGHT);
+  }
+};
+
+const handleScroll = () => {
+  const navigation = document.querySelector(`.${HEADER_CLASSNAME}`);
+  const action = window.scrollY >= STICKY_HEADER_TRIGGER ? 'add' : 'remove';
+  navigation.classList[action](STICKY_HEADER_CLASSNAME);
+};
+
+const handleScrollUp = () => {
+  verticalSmoothScroll(0);
+};
+
+const handleSkipPromo = () => {
+  const promoHeight = document.querySelector(PROMO_BLOCK).clientHeight;
+  verticalSmoothScroll(promoHeight - STICKY_HEADER_HEIGHT);
+};
+
+const handleToggleMenu = () => {
+  const headerNav = document.querySelector(HEADER_NAV);
+  const headerNavToggle = document.querySelector(NAV_TOGGLE_ICON);
+  headerNavToggle.classList.toggle('active');
+  headerNav.classList.toggle('open');
+};
+
+const handleNavigation = (e) => {
+  e.preventDefault();
+  scrollToSection(e.target.hash);
+};
+
+const handleGoToBtn = (e) => {
+  const sel = e.target.id.replace('btn-', '#');
+  scrollToSection(sel);
+};
 
 if (hasTouch) {
   document.body.classList.add('touchscreen');
 }
 
-const handleScroll = () => {
-  const navigation = document.querySelector(`.${HEADER_CLASSNAME}`);
-  const action = (window.scrollY >= STICKY_HEADER_TRIGGER) ? 'add' : 'remove';
-  navigation.classList[action](STICKY_HEADER_CLASSNAME);
-};
-
-const handleScrollUp = () => {
-  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-};
-
-const handleSkipPromo = () => {
-  const promoHeight = document.querySelector('.promo').clientHeight;
-  window.scrollTo({ top: promoHeight - STICKY_HEADER_HEIGHT, left: 0, behavior: 'smooth' });
-};
-
 window.addEventListener('scroll', handleScroll);
 window.addEventListener('load', handleScroll);
 
-const skipPromo = document.querySelector('.promo__arrow-down');
-if (skipPromo) {
-  skipPromo.addEventListener('click', handleSkipPromo);
-}
-
-const upArrow = document.querySelector(UP_BUTTON_CLASSNAME);
-if (upArrow) {
-  upArrow.addEventListener('click', handleScrollUp);
-}
-
+addEvent(DOWN_BUTTON_CLASSNAME, 'click', handleSkipPromo);
+addEvent(UP_BUTTON_CLASSNAME, 'click', handleScrollUp);
+addEvent(NAV_TOGGLE_ICON, 'click', handleToggleMenu);
+addEvent(HEADER_NAV, 'click', handleNavigation);
+addEvent(BTN_TERMS, 'click', handleGoToBtn);
+addEvent(BTN_CONTACTS, 'click', handleGoToBtn);
