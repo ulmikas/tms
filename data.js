ea624b127834e77ed3e9abc0958fe6547bea5d28
nikolay.buzdalov <buzdalov@restream.rt.ru>
2018-04-05 10:43:28 +0400

    category description, refactoring

--- data.js
+++ data.js
@@ -1,46 +1,13 @@
 const data = {
   categories: [
     {
-      slug: 'Screwdrivers',
+      slug: 'screwdrivers',
       catName: 'Отвёртки',
-      catImage: 'cat1.png',
-      description: 'категория 1 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
+      catImage: 'screwdrivers.jpg',
+      description: [
+        'Простой инструмент&nbsp;&mdash; отвертка имеет широкую среду применения. Отвертки для сухой и&nbsp;агрессивной среды, прецизионные отвертки для точных работ, варианты с&nbsp;деревянной ручкой или с&nbsp;керамическим жалом, динамометрические или антистатические. Все это вы&nbsp;найдете в&nbsp;ассортименте отверток Wiha.',
+        'Так-же у&nbsp;нас имеются наборы экстракторов&nbsp;&mdash; инструментов для безопасного удаления поврежденных или обломанных винтов.',
       ],
-    },
-    {
-      slug: 'voltage-probes',
-      catName: 'Пробники напряжения',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'blade-systems',
-      catName: 'Система комбинированных жал',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
       products: [
         {
           name: 'товар 1',
@@ -57,44 +24,8 @@ const data = {
     {
       slug: 'l-keys',
       catName: 'Шестигранные ключи',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'bits',
-      catName: 'Биты',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'drill-bits',
-      catName: 'Сверло-биты',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'l-keys.jpg',
+      description: ['Шестигранные ключи WIHA - это профессиональный инструмент с эргономичными формами рукояток и применением современных технологий для повышения качества работы. Предназначены для работы с крепежом различных профилей.'],
       products: [
         {
           name: 'товар 1',
@@ -111,8 +42,44 @@ const data = {
     {
       slug: 'pliers',
       catName: 'Шарнирно-Губцевый инструмент',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'pliers.jpg',
+      description: ['Шарнирно-губцевый инструмент Wiha - качественный и эргономичный инструмент, ориентированный на потребности пользователя. Многокомпонентные ручки с расширенной наружной частью рукоятки и продуманным распределением твердых и мягких зон сделают вашу работу комфортной.'],
+      products: [
+        {
+          name: 'товар 1',
+          description: 'описание продукта 1',
+          image: 'product1.jpg',
+        },
+        {
+          name: 'товар 2',
+          description: 'описание продукта 2',
+          image: 'product2.jpg',
+        },
+      ],
+    },
+    {
+      slug: 'bits',
+      catName: 'Биты',
+      catImage: 'bits.jpg',
+      description: ['Высококачественные биты Wiha - используются как вручную с битодержателями или рукоятками, так и при работе с шуруповертами/винтовертами. Разнообразный выбор профилей и хвостовиков гарантирует точный подбор необходимого вам инструмента.'],
+      products: [
+        {
+          name: 'товар 1',
+          description: 'описание продукта 1',
+          image: 'product1.jpg',
+        },
+        {
+          name: 'товар 2',
+          description: 'описание продукта 2',
+          image: 'product2.jpg',
+        },
+      ],
+    },
+    {
+      slug: 'hammers',
+      catName: 'Молотки',
+      catImage: 'hammers.jpg',
+      description: ['Молотки WIHA - это превосходный инструмент, обеспечивающий оптимальную передачу силы удара и гарантирующий безопасность и комфорт при работе.'],
       products: [
         {
           name: 'товар 1',
@@ -129,62 +96,8 @@ const data = {
     {
       slug: 'vernier-callipers',
       catName: 'Измерительный инструмент',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'foldings',
-      catName: 'Складные метры',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'special-tools',
-      catName: 'Уровни',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
-      products: [
-        {
-          name: 'товар 1',
-          description: 'описание продукта 1',
-          image: 'product1.jpg',
-        },
-        {
-          name: 'товар 2',
-          description: 'описание продукта 2',
-          image: 'product2.jpg',
-        },
-      ],
-    },
-    {
-      slug: 'tweezers',
-      catName: 'Пинцеты',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'vernier-callipers.jpg',
+      description: ['Измерительные инструменты WIHA - практичные и удобные в обращении инструменты, устойчивые к разного рода физическим воздействиям и обладающие повышенным сроком службы.'],
       products: [
         {
           name: 'товар 1',
@@ -201,8 +114,8 @@ const data = {
     {
       slug: 'nut-drivers',
       catName: 'Ключи и трещётки',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'nut-drivers.jpg',
+      description: ['Ключи с трещёткой WIHA - практичные и удобные в обращении инструменты, устойчивые к разного рода физическим воздействиям и обладающие повышенным сроком службы.'],
       products: [
         {
           name: 'товар 1',
@@ -219,8 +132,8 @@ const data = {
     {
       slug: 'crimping-tools',
       catName: 'Обжимной инструмент',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'crimping-tools.jpg',
+      description: ['Обжимные ключи WIHA - практичные и удобные в обращении инструменты, устойчивые к разного рода физическим воздействиям и обладающие повышенным сроком службы.'],
       products: [
         {
           name: 'товар 1',
@@ -237,8 +150,8 @@ const data = {
     {
       slug: 'sets',
       catName: 'Наборы инструментов',
-      catImage: 'cat-image2.jpg',
-      description: 'категория 2 описание',
+      catImage: 'sets.jpg',
+      description: ['Наборы инструментов WIHA – это широкий набор различных инструментов под рукой, позволяющие решать сразу несколько задач как в производстве, так и в быту.'],
       products: [
         {
           name: 'товар 1',
